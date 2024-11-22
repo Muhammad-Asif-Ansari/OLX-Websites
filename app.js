eb9d09c32c15107045615ee4ed1d25f7adc8742e
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2024-11-22 08:19:47 +0500

    olx
    olx

---
+++ app.js
@@ -0,0 +1,55 @@
+var cart = document.getElementById("cart-3")
+var mobileCart = [
+    {
+        Rs:90_00_0,
+        mobileName:"oneplus 9 pro 12gb Ram 256 GB memory back side small crack",
+        location:"bank road mardan",
+        brand:"oneplus",
+        condition:"used",
+        details:function(){
+            return`${this.Rs}  ${this.mobileName} ${this.location} ${this.brand}`
+        }
+    
+    },
+    {
+        Rs:90_00_0,
+        mobileName:"oneplus 9 pro 12gb Ram 256 GB memory back side small crack",
+        location:"bank road mardan",
+        brand:"oneplus",
+        condition:"used",
+        details:function(){
+            return`${this.Rs}  ${this.mobileName} ${this.location} ${this.brand}`
+        }
+    
+    },
+    {
+        Rs:90_00_0,
+        mobileName:"oneplus 9 pro 12gb Ram 256 GB memory back side small crack",
+        location:"bank road mardan",
+        brand:"oneplus",
+        condition:"used",
+        details:function(){
+            return`${this.Rs}  ${this.mobileName} ${this.location} ${this.brand}`
+        }
+    
+    },
+    {
+        Rs:90_00_0,
+        mobileName:"oneplus 9 pro 12gb Ram 256 GB memory back side small crack",
+        location:"bank road mardan",
+        brand:"oneplus",
+        condition:"used",
+        details:function(){
+           return`${this.Rs}  ${this.mobileName} ${this.location} ${this.brand}`
+        }
+    
+    }
+]
+
+console.log(mobileCart[0].details());
+console.log(mobileCart[1].details());
+console.log(mobileCart[2].details());
+console.log(mobileCart[3].details());
+
+
+
